12374266999296a8e7c8ede6f3db1aa3cc836824
Eric Biggs <eric@shyapply.com>
2024-08-10 21:41:15 -0400

    Ensure we have more languages installed

--- lua/proziam/plugins/treesitter.lua
+++ lua/proziam/plugins/treesitter.lua
@@ -3,7 +3,22 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'vim', 'vimdoc', 'svelte', 'htmldjango' },
+      ensure_installed = {
+        'bash',
+        'c',
+        'diff',
+        'html',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'vim',
+        'vimdoc',
+        'svelte',
+        'typescript',
+        'javascript',
+        'htmldjango',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -17,9 +32,10 @@ return {
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-      require('nvim-treesitter.install').compilers = { 'cc', 'gcc', 'clang', 'cl', 'zig' }
+      require('nvim-treesitter.install').compilers = { 'zig' }
+      -- require('nvim-treesitter.install').compilers = { 'cc', 'gcc', 'clang', 'cl', 'zig' }
       -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
+      require('nvim-treesitter.install').prefer_git = false
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
 
