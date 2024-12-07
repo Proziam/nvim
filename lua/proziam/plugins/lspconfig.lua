d64174f96a82e47dfcb968c90398bf12476dc221
Eric Biggs <eric@proziam.com>
2024-12-07 07:58:13 -0500

    Temporary fix for rust_analyzer errors

--- lua/proziam/plugins/lspconfig.lua
+++ lua/proziam/plugins/lspconfig.lua
@@ -87,14 +87,7 @@ return {
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         svelte = {},
-        rust_analyzer = {
-          settings = {
-            -- Added to hopefully resolve an LSP crashing issue
-            diagnostic = {
-              refreshSupport = false,
-            },
-          },
-        },
+        rust_analyzer = {},
         html = {
           filetypes = { 'html', 'htmldjango' },
           capabilities = capabilities,
